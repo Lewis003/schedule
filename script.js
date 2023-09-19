f1a2fcaee9f64ce37682d3bc6458244772110173
Lewis <muindilewis@zetech.ac.ke>
2023-09-19 11:42:26 +0300

    New Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+}
+
